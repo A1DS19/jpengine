dcb720b17d92d31cdd73bc016e8bd5da3e21a78e
Jose Padilla <jp06171@gmail.com>
2026-03-21 14:59:09 -0600

    remove temp

--- temp/drawing-sdl/assets/scripts/main.lua
+++
@@ -1,8 +0,0 @@
-print("running lua script")
-
-function move_box()
-	local x = get_x_cpp()
-	local y = get_y_cpp()
-	move_box_cpp(x + 1, y + 1)
-end
-
--- temp/test-emscriten/include/main.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <string_view>
-
-void hello();
-void greet(std::string_view name);
